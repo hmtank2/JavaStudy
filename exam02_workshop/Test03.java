c89dc8f54591eb8f3471f5b34d5c8b8fc5d5c076
Hyuk Min Yang <hmtank2@gmail.com>
2023-07-13 23:54:07 +0900

    2중 for문 2중 while 문으로.

---
+++ exam02_workshop/Test03.java
@@ -0,0 +1,23 @@
+package exam02_workshop;
+
+public class Test03 {
+	public static void main(String[] args) {
+		int i=1;
+		
+		while(i<=10)
+		{
+			int n=1;
+			while(n<=i) {
+				System.out.print("*");
+				n++;
+			}
+			
+			System.out.println();
+			i++;
+		}
+	}
+}
+			
+		
+	
+
