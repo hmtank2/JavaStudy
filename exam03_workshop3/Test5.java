a66193a4965cf2036215b9249863199079e9e6d0
Hyuk Min Yang <hmtank2@gmail.com>
2023-07-14 23:44:37 +0900

    2차원 배열 인덱스의 역순으로 출력해봄. for문에서 i--;등의 방식을 씀.

---
+++ exam03_workshop3/Test5.java
@@ -0,0 +1,24 @@
+package workshop03;
+
+public class Test5 {
+	public static void main(String[] args) {
+		int[][] arr2 = {
+				 {20, 30, 10},
+				 {50, 40, 60},
+				 {80, 80, 90}
+				 };
+
+
+		int sum = 0;
+		float cnt =0;
+		for(int i=arr2.length-1; i>=0 ;i--) {
+			for(int j=arr2[i].length-1; j>=0 ;j--) {
+				System.out.print(arr2[i][j]+" ");
+			}
+//				System.out.println("");
+			}
+			
+		}
+		
+	
+}
