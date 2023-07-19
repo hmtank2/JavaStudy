9f800fbf0274bae211f42c5c92269dc8ee90b4a9
Hyuk Min Yang <hmtank2@gmail.com>
2023-07-19 23:24:35 +0900

    vehicle 을 상속하는 클래스. 남은 연료와 현재 적재량이 멤버 변수로 선언됨. addOil(), moving(), addWeight() 도 여기서 처음 선언됨. 이때 moving은 truck class에서 오버로딩되어서 조금 다른 동작을 하므로 유의

---
+++ workshop06/vehicle/Car.java
@@ -0,0 +1,48 @@
+package vehicle;
+
+public class Car extends Vehicle {
+	private  double restOil;
+	private  int curWeight;
+	public Car() {
+	}
+	public Car(int maxWeight, double oilTankSize, double efficiency) {
+		super(maxWeight,  oilTankSize, efficiency);
+	}
+	
+	public void addOil(int oil) {
+		if(oil+ this.getRestOil() < this.getOilTankSize()){
+			restOil+=oil;
+		}
+	}
+	
+	public void moving(int distance) {
+		this.restOil -= (distance * this.getEfficiency());
+	}
+	public void addWeight(int weight) {
+		if(weight + this.getCurWeight() <this.getMaxWeight()) {
+			this.curWeight+=weight;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		
+		return super.toString()+"\t"+this.restOil +"\t"+this.curWeight;
+	}
+	
+		
+	public double getRestOil() {
+		return restOil;
+	}
+	public void setRestOil(double restOil) {
+		this.restOil = restOil;
+	}
+	public int getCurWeight() {
+		return curWeight;
+	}
+	public void setCurWeight(int curWeight) {
+		this.curWeight = curWeight;
+	}
+	
+	
+}
